d07e4c25ca8d24693b99b1633a6991e40a6168db
Arthur Guillemin <aguillemin291@gmail.com>
2025-03-24 21:45:13 +0100

    fix path in laundry component

--- Dashboard-front/src/components/laundry/laundry.jsx
+++ Dashboard-front/src/components/laundry/laundry.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import styles from "./Laundry.module.css";
+import styles from "./laundry.module.css";
 
 const Laundry = () => {
   const [shouldWash, setShouldWash] = useState(false);
